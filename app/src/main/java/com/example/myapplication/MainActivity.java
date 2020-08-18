98aee475cbf30f2607ccecb953f140833e841c5e
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-18 10:51:21 +0900

    [Add] func onClicked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View v) {
+        Toast.makeText(this, "버튼이 눌러졌어요.",Toast.LENGTH_LONG).show();
     }
 }
