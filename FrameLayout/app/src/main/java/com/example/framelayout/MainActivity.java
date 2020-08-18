00a89f9f084ed3f5b33aa7f53b12c590dcab0d22
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-18 14:11:09 +0900

    [Add] Img, Btn

--- FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.java
+++ FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.framelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img1;
+    ImageView img2;
+    int state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img1 = findViewById(R.id.cloud1);
+        img2 = findViewById(R.id.cloud2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if (state == 0) {
+                    img1.setVisibility(View.INVISIBLE);
+                    img2.setVisibility(View.VISIBLE);
+                    state = 1;
+                } else {
+                    img1.setVisibility(View.VISIBLE);
+                    img2.setVisibility(View.INVISIBLE);
+                    state = 0;
+                }
+
+            }
+        });
     }
 }
