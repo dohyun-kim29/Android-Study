0cd0853e9bb5ea12a9715b57d1094752b63514be
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-18 11:58:59 +0900

    [Add] basicUI

--- Activity/app/src/main/java/com/example/testactivity/MainActivity.java
+++ Activity/app/src/main/java/com/example/testactivity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.testactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,7 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
 
-        Button button = findViewById(R.id.button)
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("안녕하세요 김도현입니다.");
+            }
+        });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setTextColor(Color.RED);
+            }
+        });
     }
 }
