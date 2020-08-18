37666631ce398c5e7660d809e5b8302546a368c0
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-18 11:14:46 +0900

    [Add] connectNaver, call

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClicked(View v) {
         Toast.makeText(this, "버튼이 눌러졌어요.",Toast.LENGTH_LONG).show();
+    }
+
+    public void connectNaver(View v) {
+        Toast.makeText(this, "네이버에 연결합니다.", Toast.LENGTH_LONG).show();;
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void call(View v) {
+        Toast.makeText(this, "전화를 연결합니다.", Toast.LENGTH_LONG).show();
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-0000-0000"));
+        startActivity(myIntent);
     }
 }
