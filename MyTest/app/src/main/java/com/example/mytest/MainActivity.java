e1f483db24cea8d298b078b56d62563fbbfded1b
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-19 11:14:45 +0900

    [Add] Android Project

--- MyTest/app/src/main/java/com/example/mytest/MainActivity.java
+++ MyTest/app/src/main/java/com/example/mytest/MainActivity.java
@@ -3,15 +3,48 @@ package com.example.mytest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.textView);
+        View view = findViewById(R.id.view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int action = motionEvent.getAction();
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+                    printIn("Down: " + curX "." +curY);
+                } else if (action == MotionEvent.ACTION_MOVE) {
+                   printIn("Move: " + curX "." + );
+                } else if (action == MotionEvent.ACTION_UP) {
+                    printIn("UP");
+                }
+
+                return false;
+
+
+            }
+        });
+
+    }
+
+    public void printIn(stting data) {
+        textView.append(data +);
     }
 }
