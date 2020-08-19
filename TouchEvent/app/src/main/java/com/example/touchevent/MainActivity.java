4de597c3cab6a388ac76225a2ecf6260c13ad3dc
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-19 11:57:55 +0900

    [Add] logic

--- TouchEvent/app/src/main/java/com/example/touchevent/MainActivity.java
+++ TouchEvent/app/src/main/java/com/example/touchevent/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.touchevent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = findViewById(R.id.imageView);
+
+        View view = findViewById(R.id.view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                int action = motionEvent.getAction();
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+
+                if (action == MotionEvent.ACTION_DOWN) {
+
+                img.setVisibility(View.VISIBLE);
+                img.setX(curX);
+                img.setY(curY);
+                }
+                return true;
+            }
+        });
     }
 }
