067222d49d7185158a7b0024f5fb673a011aeeb1
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-19 16:02:05 +0900

    [Add] logic

--- MyInflater/MyInflater2/app/src/main/java/com/example/myinflater/MainActivity.java
+++ MyInflater/MyInflater2/app/src/main/java/com/example/myinflater/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.myinflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       container = findViewById(R.id.container);
+        Button button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub, container, true);
+
+                CheckBox checkBox = findViewById(R.id.checkBox2);
+                checkBox.setText("확인");
+            }
+        });
+
     }
 }
