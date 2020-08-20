97779dbdb6c94096d9e31e258cae1d5b73351258
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-20 12:13:30 +0900

    [Add] SimpleData Class

--- MyParcelable/app/src/main/java/com/example/myparcelable/MenuActivity.java
+++ MyParcelable/app/src/main/java/com/example/myparcelable/MenuActivity.java
@@ -3,6 +3,8 @@ package com.example.myparcelable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
---
+++ MyParcelable/app/src/main/java/com/example/myparcelable/SimpleData.java
@@ -0,0 +1,4 @@
+package com.example.myparcelable;
+
+public class SimpleData {
+}
