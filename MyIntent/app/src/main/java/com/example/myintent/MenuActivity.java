60af3c8563436d39600534d6e573830e1c0dc9f2
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-20 10:59:37 +0900

    [Add] MenuActivity

--- MyIntent/app/src/main/java/com/example/myintent/MenuActivity.java
+++ MyIntent/app/src/main/java/com/example/myintent/MenuActivity.java
@@ -7,21 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
+
     }
 }
