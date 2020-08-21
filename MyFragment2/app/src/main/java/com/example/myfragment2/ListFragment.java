c9f5fa2b90789c3c6e46cbf9bd3ce28b459d83e4
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-21 09:48:53 +0900

    [Add] AndroidProject

--- MyFragment2/app/src/main/java/com/example/myfragment2/ListFragment.java
+++ MyFragment2/app/src/main/java/com/example/myfragment2/ListFragment.java
@@ -1,21 +1,73 @@
 package com.example.myfragment2;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ListFragment extends Fragment {
 
-
+    public static interface ImageSelectionCallback {
+        public void onImageSelected(int position);
+    }
+    public ImageSelectionCallback callback;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        ViewGroup rootView =
+                (ViewGroup) inflater.inflate(R.layout.fragment_viewer, container, false);
+
+
+        Button button1 = rootView.findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) {
+                    callback.onImageSelected(0);
+                }
+
+            }
+        });
+
+        Button button2 = rootView.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) {
+                    callback.onImageSelected(1);
+                }
+
+            }
+        });
+
+        Button button3 = rootView.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null) {
+                    callback.onImageSelected(2);
+                }
+
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        if (context instanceof  ImageSelectionCallback) {
+            callback = (ImageSelectionCallback) context;
+        }
     }
 }
--- MyFragment2/app/src/main/java/com/example/myfragment2/MainActivity.java
+++ MyFragment2/app/src/main/java/com/example/myfragment2/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.myfragment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.ImageSelectionCallback {
+
+    ListFragment listFragment;
+    ViewerFragment viewerFragment;
+
+    int[] images = {R.drawable.blueberry, R.drawable.raspberry, R.drawable.grapes};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        listFragment = (ListFragment) manager.findFragmentById(R.id.listFragment);
+        viewerFragment = (ViewerFragment) manager.findFragmentById(R.id.viewerFragment);
+    }
+
+    @Override
+    public void onImageSelected(int position) {
+        viewerFragment.setImage(images[position]);
     }
 }
