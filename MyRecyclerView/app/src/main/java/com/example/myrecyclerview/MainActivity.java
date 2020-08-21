3ee2e3956a2082940f77922505b858fb91e28918
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-21 15:44:22 +0900

    [Add] OnPersonItemClickListener

--- MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        PersonAdapter adapter = new PersonAdapter();
+        adapter.addItem(new Person("김도현", "010-1111-1111"));
+        adapter.addItem(new Person("이도현", "010-2222-2222"));
+        adapter.addItem(new Person("박도현", "010-3333-3333"));
+
+        recyclerView.setAdapter(adapter);
+
     }
 }
