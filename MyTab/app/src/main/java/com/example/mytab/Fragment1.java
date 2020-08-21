29476b632a97294b677f5a60d66458b6c0a80e6b
DohyunKIm <rlaehgus1229@gmail.com>
2020-08-21 12:11:03 +0900

    [Add] logic at Frag1

--- MyTab/app/src/main/java/com/example/mytab/Fragment1.java
+++ MyTab/app/src/main/java/com/example/mytab/Fragment1.java
@@ -1,5 +1,6 @@
 package com.example.mytab;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Fragment1 extends Fragment {
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_1, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+
+
+        Button button1 = rootView.findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootView.setBackgroundColor(Color.RED);
+            }
+        });
+        return rootView;
     }
 }
